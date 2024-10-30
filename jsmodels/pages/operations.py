9ca9ea5b816aa77ffff449b5afce07f8d77c4d94
User <user@Rorie.local>
2024-10-30 00:32:45 -0400

    Fixed a bug with hash generation

--- jsmodels/pages/operations.py
+++ jsmodels/pages/operations.py
@@ -1,18 +1,28 @@
 import json
+from datetime import datetime
 from pages.models import SharedModelData
 import hashlib
 
 def save_model(post_data):
 	model_data = json.loads(post_data)
 	model_instance = SharedModelData()
+	current_date = datetime.now()
+	datetime_with_ms = current_date.strftime('%Y-%m-%d %H:%M:%S.%f')
 	model_instance.document_id = SharedModelData.objects.latest('document_id').document_id + 1
 	model_instance.input_vars = model_data["inputs"]
 	model_instance.model_code = model_data["code"]
 	model_instance.output_vars = model_data["outputs"] 
-	model_instance.model_hash = hashlib.sha256(model_data["code"].encode('utf-8')).hexdigest()
+	model_instance.model_hash = get_model_hash(model_data["code"])
 	model_instance.save()
 	return model_instance.model_hash
 
+
+def get_model_hash(str_code):
+	current_date = datetime.now()
+	datetime_with_ms = current_date.strftime('%Y-%m-%d %H:%M:%S.%f')
+	string_to_be_hashed = str_code + datetime_with_ms
+	hash_value = hashlib.sha256(string_to_be_hashed.encode('utf-8')).hexdigest()
+	return hash_value
 
 def load_model(model_id):
 	print(model_id)
