592746271b195176a668d88b507628dc98f58981
User <user@Rorie.local>
2024-11-09 20:27:03 -0500

    fixed trailing slash issue

--- jsmodels/pages/urls.py
+++ jsmodels/pages/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, re_path
 
 
 urlpatterns = [
-    path("model",views.home, name='home'),
+    path("model/",views.home, name='home'),
     path("model/get_data", views.api, name='api'),
     re_path(r'^model/(?P<model_id>[a-f0-9]+)$', views.home, name='home'),
 
