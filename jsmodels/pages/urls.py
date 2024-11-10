74c302f07e2c1fa01b8f6a0919c4f055ec0afe45
User <user@Rorie.local>
2024-11-09 21:39:26 -0500

    Redirect implemented

--- jsmodels/pages/urls.py
+++ jsmodels/pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from pages import views
 from django.urls import path, re_path
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    path("",RedirectView.as_view(pattern_name="home", permanent=False)),
     path("model/",views.home, name='home'),
     path("model/get_data", views.api, name='api'),
     re_path(r'^model/(?P<model_id>[a-f0-9]+)$', views.home, name='home'),
